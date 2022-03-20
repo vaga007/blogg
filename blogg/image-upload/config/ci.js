a718de4aed8865f64cd23e4162724cd6a9693558
Raktima Saxena <raktima.saxena@gmail.com>
2022-03-20 22:27:51 +0530

    aws sdk and image upload

---
+++ blogg/image-upload/config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: 'mongodb://127.0.0.1:27017/Blogg',
+    cookieKey: process.env.COOKIE_KEY,
+    redisUrl: 'redis://127.0.0.1:6379'
+  };
+  
